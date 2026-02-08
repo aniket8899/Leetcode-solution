31b8f54ded60e6ddba0dc2d17a6a3aa1cfc77a66
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-02-09 03:19:29 +0530

    Time: 3 ms (68.14%), Space: 14.9 MB (18.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,vector<int>>mp;
+        // unordered_map<int,vector<int>>mp;
        
         // for(int i=0;i<nums.size();i++){
         //     int number = nums[i];
@@ -16,32 +16,56 @@ public:
 
     // method--->>2
 
-    for(int i=0;i<nums.size();i++)
-    {
-        int val=nums[i];
-        mp[val].push_back(i);
-    }
+    // for(int i=0;i<nums.size();i++)
+    // {
+    //     int val=nums[i];
+    //     mp[val].push_back(i);
+    // }
 
-    for(int i=0;i<nums.size();i++)
-    {
-        int current=nums[i];
-        int req=target-current;
+    // for(int i=0;i<nums.size();i++)
+    // {
+    //     int current=nums[i];
+    //     int req=target-current;
 
-        if(mp.find(req) !=mp.end())
-        {
-            if(req==current && mp[current].size()==1)
-            {
-                continue;
-            }
+    //     if(mp.find(req) !=mp.end())
+    //     {
+    //         if(req==current && mp[current].size()==1)
+    //         {
+    //             continue;
+    //         }
 
-            if(req==current && mp[current].size()>1)
-            {
-                return {mp[current][0],mp[current][1]};
-            }
-            else
-            {
-                return {i,mp[req][0]};
-            }
+    //         if(req==current && mp[current].size()>1)
+    //         {
+    //             return {mp[current][0],mp[current][1]};
+    //         }
+    //         else
+    //         {
+    //             return {i,mp[req][0]};
+    //         }
+    //     }
+    // }
+    // return {};
+    int n=nums.size();
+  vector<pair<int,int>>mp;
+   for(int i=0;i<n;i++){
+        mp.push_back({nums[i],i});
+   }
+   sort(mp.begin(),mp.end());
+  
+    int left=0,right=n-1;
+    
+    
+
+    while(left<right){
+        int sum=mp[left].first+mp[right].first;
+        if(sum==target){
+            return {mp[left].second,mp[right].second};
+          
+        }
+        else if(sum<target){
+            left++;
+        }else{
+            right--;
         }
     }
     return {};
