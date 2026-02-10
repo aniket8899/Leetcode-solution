886559f911952305f4fe50fa92573444f1d5f0dc
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-02-10 15:16:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.31%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,21 +3,26 @@ public:
     int mySqrt(int x) {
         int s=0;
         int e=x;
-        long long int mid =s+(e-s)/2;
         int ans=-1;
+
         while(s<=e){
-            long long product =mid*mid;
+            long long int mid=s+(e-s)/2;
+            long long product=mid*mid;
+
             if(product==x){
                 return mid;
             }
-            if(product<x){
-                ans=mid;
-                s=mid+1;
-            }else{
+            else if(product>x){
+               
                 e=mid-1;
+                
             }
-            mid =s+(e-s)/2;
+            else{
+                 ans=mid;
+                s=mid+1;
+            }
         }
         return ans;
+        
     }
 };
