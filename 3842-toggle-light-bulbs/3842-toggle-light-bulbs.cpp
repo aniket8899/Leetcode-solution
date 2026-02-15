4d4b74841313161d90e76602a78f32751f87c2d6
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-02-15 17:15:44 +0530

    Time: 3 ms (62.03%), Space: 34.6 MB (34.95%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -3,15 +3,20 @@ public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
         vector<int>ans;
         int n=bulbs.size();
+       
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             mp[bulbs[i]]++;
         }
-        for(auto it:mp){
-            if(it.second==1){
-                ans.push_back(it.first);
-            }
+       for(auto it:mp){
+        if((it.second%2)==1){
+            ans.push_back(it.first);
         }
+       }
+       sort(ans.begin(),ans.end());
+        
         return ans;
+
+        
     }
 };
