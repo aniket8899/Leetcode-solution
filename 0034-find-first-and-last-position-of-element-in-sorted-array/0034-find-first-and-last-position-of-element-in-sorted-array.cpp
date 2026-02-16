3c7611415210f908bb951d4e1cca368125d7a1b6
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-02-16 22:39:33 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.8%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,60 +1,47 @@
 class Solution {
 public:
-void findfirstoccurrence(vector<int>arr,int n,int target,int &ans){
-    int s=0;
-    int e=n-1;
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==target){
-            //ans found-> may or may not be first occurrence
-            //store and compute
-         ans=mid;
-         //kyuki first occurrence ki baat hori hai
-         //toh batao left me jau first occ ka liya ya right me
-         //left me jao
-         e=mid-1;
-
-        }
-        if(target> arr[mid]){
-            s=mid+1;
-        }
-        if(target<arr[mid]){
-            e=mid-1;
-        }
-         mid=s+(e-s)/2;
-    }
-}
-
-void findlastoccurrence(vector<int>arr,int n,int target,int &ans){
-    int s=0,e=n-1;
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==target){
-            ans=mid;
-            s=mid+1;
-        }
-        if(target>arr[mid]){
-            //right me jao
-            s=mid+1;
-        }
-        if(target<arr[mid]){
-            //left me jao
-            e=mid-1;
-        }
-        mid=s+(e-s)/2;
-    }
-}
-    vector<int> searchRange(vector<int>& arr, int target) {
+    void firstoccurence(vector<int>&arr,int target,int& firstocc){
         int n=arr.size();
-        int firstoccurrence=-1;
-        findfirstoccurrence(arr,n,target,firstoccurrence);
+        int st=0;
+        int end=n-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(arr[mid]==target){
+                firstocc=mid;
+               end=mid-1;
+            }else if(target>arr[mid]){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
 
-        int lastoccurrence=-1;
-        findlastoccurrence(arr,n,target,lastoccurrence);
-        vector<int>temp(2);
-        temp[0]=firstoccurrence;
-        temp[1]=lastoccurrence;
-        return temp;
-        
+    }
+    void secondoccurence(vector<int>&arr,int target,int& secondocc){
+       int n=arr.size();
+        int st=0;
+        int end=n-1;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(arr[mid]==target){
+                secondocc=mid;
+                st=mid+1;
+            }else if(target>arr[mid]){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>ans(2);
+        int firstocc=-1;
+        firstoccurence(nums,target,firstocc);
+        int secondocc=-1;
+        secondoccurence(nums,target,secondocc);
+        ans[0]=firstocc;
+        ans[1]=secondocc;
+
+        return ans;
     }
 };
