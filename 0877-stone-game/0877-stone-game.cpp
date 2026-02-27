965e29255101da825c14fa527fe99bf0ed3ca75b
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-02-28 00:29:18 +0530

    Time: 0 ms (100%), Space: 10.5 MB (30.23%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // alice ka pass 2 option hai choose karna ki 
+        // agr even index lekar sum bada aata hai toh ham even index lenge 
+        // nhi to odd index choose karenga
+        // alice ka paass wo opti0n hai choose karna ka 
+        return true;
+    }
+};
