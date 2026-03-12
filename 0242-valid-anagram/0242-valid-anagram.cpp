cff45a07b05292b04d94b9d42222220c334dbb6d
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-03-13 01:06:02 +0530

    Time: 8 ms (7.31%), Space: 9.7 MB (73.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    //   int freqTable[256]={0};
+    //   for(int i=0;i<s.size();i++){
+    //     freqTable[s[i]]++;
+    //   }
+    //   for(int i=0;i<t.size();i++){
+    //      freqTable[t[i]]--;
+    //   }
+    //   for(int i=0;i<256;i++){
+    //     if(freqTable[i]!=0){
+    //         return false;
+    //     }
+    //   }
+    //     return true;
+
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+
+    int n=s.size();
+    int m=t.size();
+    if(n!=m) return false;
+     for(int i=0;i<n && i<m;i++){
+        if(s[i]!=t[i]){
+            return false;
+        }
+     }
+     return true;
+    }
+};
