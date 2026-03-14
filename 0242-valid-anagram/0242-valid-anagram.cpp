61431ee5e4bdc8a392ebe1d69fe6aa3be8f560d2
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-03-14 15:07:43 +0530

    Time: 5 ms (27.13%), Space: 10.5 MB (10.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -15,17 +15,34 @@ public:
     //   }
     //     return true;
 
-    sort(s.begin(),s.end());
-    sort(t.begin(),t.end());
+    // sort(s.begin(),s.end());
+    // sort(t.begin(),t.end());
 
-    int n=s.size();
-    int m=t.size();
-    if(n!=m) return false;
-     for(int i=0;i<n && i<m;i++){
-        if(s[i]!=t[i]){
+    // int n=s.size();
+    // int m=t.size();
+    // if(n!=m) return false;
+    //  for(int i=0;i<n && i<m;i++){
+    //     if(s[i]!=t[i]){
+    //         return false;
+    //     }
+    //  }
+    //  return true;
+
+
+   unordered_map<char,int>freq;
+
+    for(int i=0;i<s.size();i++){
+        freq[s[i]]++;
+    }
+    for(int i=0;i<t.size();i++){
+        freq[t[i]]--;
+
+    }
+    for(int i=0;i<256;i++){
+        if(freq[i]!=0){
             return false;
         }
-     }
-     return true;
+    }
+    return true;
     }
 };
