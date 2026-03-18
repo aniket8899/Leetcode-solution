ee42116e24b96e08a8b35a2692b401e34e10e691
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-03-18 22:30:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,17 +2,39 @@ class Solution {
 public:
     bool isPalindrome(string s) {
 
-        string ans="";
-        for(auto ch:s){
-            if(isalnum(ch)){
-                ans.push_back(tolower(ch));
+        // string ans="";
+        // for(auto ch:s){
+        //     if(isalnum(ch)){
+        //         ans.push_back(tolower(ch));
+        //     }
+        // }
+
+        // string s1=ans;
+        // reverse(ans.begin(),ans.end());
+
+        // return s1==ans;
+
+
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
+            }
+
+            else if(!isalnum(s[j])){
+                j--;
+            }
+
+             else if(tolower(s[i])!=tolower(s[j])){
+                return false;
+            }else{
+                i++,j--;
             }
         }
-
-        string s1=ans;
-        reverse(ans.begin(),ans.end());
-
-        return s1==ans;
+        return true;
 
 
         
