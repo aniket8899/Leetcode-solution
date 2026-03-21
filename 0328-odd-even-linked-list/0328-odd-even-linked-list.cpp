2fb75bead762efc279cfc14705bce06d243bacbc
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-03-22 01:17:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,34 +11,56 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head==NULL) return head;
-        vector<int>ans;
-        ListNode* temp=head;
+        // if(head==NULL) return head;
+        // vector<int>ans;
+        // ListNode* temp=head;
         
-        while(temp!=NULL && temp->next!=NULL){
-            ans.push_back(temp->val);
-            temp=temp->next->next;
-        }
-        if(temp){
-            ans.push_back(temp->val);
-        }
-         temp=head->next;
+        // while(temp!=NULL && temp->next!=NULL){
+        //     ans.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp){
+        //     ans.push_back(temp->val);
+        // }
+        //  temp=head->next;
 
-        while(temp!=NULL && temp->next!=NULL){
-            ans.push_back(temp->val);
-            temp=temp->next->next;
-        }
-        if(temp){
-            ans.push_back(temp->val);
-        }
+        // while(temp!=NULL && temp->next!=NULL){
+        //     ans.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp){
+        //     ans.push_back(temp->val);
+        // }
 
-        int n=ans.size();
-        temp=head;
+        // int n=ans.size();
+        // temp=head;
 
-        for(int i=0;i<n;i++){
-            temp->val=ans[i];
-            temp=temp->next;
+        // for(int i=0;i<n;i++){
+        //     temp->val=ans[i];
+        //     temp=temp->next;
+        // }
+        // return head;
+
+
+        // method--2
+
+        if(head==NULL || head->next==NULL) return head;
+
+        ListNode* odd=head;
+        ListNode* even=head->next;
+
+        ListNode* evenhead=even;
+
+        while(even!=NULL && even->next!=NULL){
+            odd->next=odd->next->next;
+
+            even->next=even->next->next;
+
+            odd=odd->next;
+            even=even->next;
         }
+        odd->next=evenhead;
+
         return head;
         
     }
