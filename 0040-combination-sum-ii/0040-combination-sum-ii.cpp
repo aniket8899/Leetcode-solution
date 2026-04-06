ed89017c33ab61c48d7ef22774648305baf4708c
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-04-07 00:36:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -8,21 +8,29 @@ public:
                 return;
             }
          
+         if(i==arr.size()) return;
 
 
     // pick
-    for(int j=i;j<arr.size();j++){
+    // for(int j=i;j<arr.size();j++){
 
         // skip duplicate
-        if(j> i && arr[j]==arr[j-1]) continue;
+        // if(j> i && arr[j]==arr[j-1]) continue;
 
-        if(arr[j]<=target){
-            ds.push_back(arr[j]);
-            solve(j+1,arr,target-arr[j],ans,ds);
+        if(arr[i]<=target){
+            ds.push_back(arr[i]);
+            solve(i+1,arr,target-arr[i],ans,ds);
             ds.pop_back();
         }
 
-    }
+        int j=i;
+        while(j+1 < arr.size() && arr[j] ==arr[j+1]) j++;
+
+        //unpick
+    solve(j+1,arr,target,ans,ds);
+
+
+    
         
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
