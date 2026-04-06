17c89dca0b36361440b3dc01ae0fb47468573d64
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-04-07 01:04:07 +0530

    Time: 0 ms (100%), Space: 10.3 MB (96.7%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(int i,vector<int>& nums,vector<vector<int>>&ans,vector<int>&ds){
+        // curr element
+        ans.push_back(ds);
+
+    for(int j=i;j<nums.size();j++){
+
+        // skip duplicate
+
+        if(j>i && nums[j]==nums[j-1]) continue;
+
+        ds.push_back(nums[j]);
+
+        solve(j+1,nums,ans,ds);
+
+        ds.pop_back();
+    }
+
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+         vector<vector<int>>ans;
+         vector<int>ds;
+
+         solve(0,nums,ans,ds);
+         return ans;
+        
+    }
+};
