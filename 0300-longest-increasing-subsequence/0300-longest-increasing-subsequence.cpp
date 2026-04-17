ef8bf5492da9c965c56945e8d5d4264611e2184d
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-04-18 00:40:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -19,10 +19,29 @@ public:
 
         return dp[i][previndex+1]= max(include,exclude);
     }
+    int optimal(vector<int>&nums){
+        int n=nums.size();
+
+        vector<int>ans;
+        ans.push_back(nums[0]);  //push first element
+
+        for(int i=1;i<n;i++){
+            if(nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+            }else{
+                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[index]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(nums,0,-1,dp);
+        // return solve(nums,0,-1,dp);
+
+        return optimal(nums);
         
     }
 };
