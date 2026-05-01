c27787f5a799c4adaa4482c0a7abf1d16d86aa0f
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-05-01 21:37:35 +0530

    Time: 7 ms (64.68%), Space: 11.4 MB (85.84%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,25 +2,102 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
-        int n=s.size();
-        int ans=0;
+    //     int n=s.size();
 
-        for(int i=0;i<n;i++){
-            unordered_set<char>st;
-            for(int j=i;j<n;j++){
-               if(st.find(s[j])!=st.end()){
-                break;
-               }
+    // int maxlen=0;
+    // for(int i=0;i<n;i++){
+    //     int hashmap[256]={0};
+    //     for(int j=i;j<n;j++){
+    //         if(hashmap[s[j]]==1){
+    //             break;
+    //         }
+    //         int len=j-i+1;
+    //         maxlen=max(maxlen,len);
 
-               st.insert(s[j]);
+    //         hashmap[s[j]]=1;
+    //     }
+    // }
 
-               int len=j-i+1;
-               ans=max(ans,len);
+    // return maxlen;
 
+
+    // sliding window
+
+    int n=s.size();
+
+    int hashmap[256];
+
+    for(int i=0;i<256;i++){
+        hashmap[i]=-1;
+    }
+
+    int l=0,r=0,maxlen=0;
+
+    while(r<n){
+        //check already map me toh nhi h
+
+        if(hashmap[s[r]]!=-1){
+            if(hashmap[s[r]]>=l){
+                l=hashmap[s[r]]+1;
+            }
         }
-        }
 
-        return ans;
+        int len=r-l+1;
+
+        maxlen=max(maxlen,len);
+
+        hashmap[s[r]]=r;
+
+        r++;
+    }
+
+
+
+
+
+
+return maxlen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int ans=0;
+
+        // for(int i=0;i<n;i++){
+        //     unordered_set<char>st;
+        //     for(int j=i;j<n;j++){
+        //        if(st.find(s[j])!=st.end()){
+        //         break;
+        //        }
+
+        //        st.insert(s[j]);
+
+        //        int len=j-i+1;
+        //        ans=max(ans,len);
+
+        // }
+        // }
+
+        // return ans;
         
     }
 };
