e3c7eb0d5c12314f9187545018683922a2149e78
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-05-09 18:50:26 +0530

    Time: 14 ms (57.35%), Space: 19 MB (9.56%) - LeetHub

--- 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+
+  vector<int>rotatedarr(vector<int>&temp,int k){
+    int n=temp.size();
+
+    k=k%n;
+
+    vector<int>ans;
+
+   // process k to n
+    for(int i=k;i<n;i++){
+        ans.push_back(temp[i]);
+    }
+
+    // first process k
+
+
+
+    for(int i=0;i<k;i++){
+        ans.push_back(temp[i]);
+    }
+
+  
+
+    return ans;
+  }
+
+
+
+
+    void processlayer(vector<vector<int>>& grid, int k,int i){
+
+
         int m=grid.size();
         int n=grid[0].size();
 
-        int strow=0;//top row
-        int endrow=m-1; // bottom row
-        int  stcol=0;// st col
-        int endcol=n-1; //endcol
+       
 
         int layer=min(m,n)/2;
 
-        for(int i=0;i<layer;i++){
-            vector<int>temp;
+       
 
             int strow=i;
             int stcol=i;
             int endrow=m-i-1;
 
             int endcol=n-i-1;
+
+            vector<int>temp;
 
             // st row
 
@@ -42,16 +71,19 @@ public:
                 temp.push_back(grid[i][stcol]);
             }
 
-            int size=temp.size();
+      vector<int>rotated=rotatedarr(temp,k);
 
-            int st=k%size;
+      int size=rotated.size();
+          
 
-            int index=st;
+          
+
+            int index=0;
 
              // st row
 
             for(int i=stcol;i<=endcol;i++){
-               grid[strow][i]=temp[index++];
+               grid[strow][i]=rotated[index++];
 
                if(size==index){
                 index=0;
@@ -62,7 +94,7 @@ public:
              // ending col
 
             for(int i=strow+1;i<=endrow-1;i++){
-                grid[i][endcol]=temp[index++];
+                grid[i][endcol]=rotated[index++];
 
                if(size==index){
                 index=0;
@@ -71,7 +103,7 @@ public:
             //ending row
 
             for(int i=endcol;i>=stcol;i--){
-                grid[endrow][i]=temp[index++];
+                grid[endrow][i]=rotated[index++];
 
                if(size==index){
                 index=0;
@@ -80,7 +112,7 @@ public:
             //st col
 
             for(int i=endrow-1;i>=strow+1;i--){
-                grid[i][stcol]=temp[index++];
+                grid[i][stcol]=rotated[index++];
 
                if(size==index){
                 index=0;
@@ -90,11 +122,20 @@ public:
 
 
 
+    }
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+       
 
+     int m=grid.size();
+        int n=grid[0].size();
+
+        int layer=min(m,n)/2;
+
+        for(int i=0;i<layer;i++){
+            processlayer(grid,k,i);
         }
 
         return grid;
-
 
 
 
