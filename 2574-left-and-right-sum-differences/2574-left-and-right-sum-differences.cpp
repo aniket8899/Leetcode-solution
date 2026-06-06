4625c1e142aabf0ccd5d885005461e1edd310ca8
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-06-06 14:42:05 +0530

    Time: 0 ms (100%), Space: 15.1 MB (46.07%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -4,36 +4,59 @@ public:
 
         int n=nums.size();
         
-        vector<int>leftsum;
-        vector<int>rightsum;
-        leftsum.push_back(0);
+        // vector<int>leftsum;
+        // vector<int>rightsum;
+        // leftsum.push_back(0);
 
-        int lsum=0;
+        // int lsum=0;
+        // // int rsum=0;
+        // for(int i=1;i<n;i++){
+        //     lsum+=nums[i-1];
+
+        //     leftsum.push_back(lsum);
+        // }
+
+        // reverse(nums.begin(),nums.end());
+
         // int rsum=0;
+        // rightsum.push_back(0);
+        // for(int i=1;i<nums.size();i++){
+        //     rsum+=nums[i-1];
+
+        //     rightsum.push_back(rsum);
+        // }
+        // reverse(rightsum.begin(),rightsum.end());
+
+
+        // vector<int>ans;
+
+        // for(int i=0;i<n;i++){
+        //     ans.push_back(abs(leftsum[i]-rightsum[i]));
+        // }
+
+        // return ans;
+
+
+        vector<int>leftsum(n,0);
+        
+
         for(int i=1;i<n;i++){
-            lsum+=nums[i-1];
-
-            leftsum.push_back(lsum);
+            leftsum[i]=leftsum[i-1]+nums[i-1];
         }
 
-        reverse(nums.begin(),nums.end());
+        //right sum
 
-        int rsum=0;
-        rightsum.push_back(0);
-        for(int i=1;i<nums.size();i++){
-            rsum+=nums[i-1];
+        vector<int>rightsum(n,0);
 
-            rightsum.push_back(rsum);
+        for(int i=n-2;i>=0;i--){
+            rightsum[i]=rightsum[i+1]+nums[i+1];
         }
-        reverse(rightsum.begin(),rightsum.end());
 
-
-        vector<int>ans;
+        vector<int>ans(n,0);
 
         for(int i=0;i<n;i++){
-            ans.push_back(abs(leftsum[i]-rightsum[i]));
+            ans[i]=abs(leftsum[i]-rightsum[i]);
         }
-
         return ans;
     }
 };
