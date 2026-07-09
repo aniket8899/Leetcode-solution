6597f97d4295b3a55d8d172bd70f5a59286f7c0a
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-07-10 00:27:22 +0530

    Time: 363 ms (5.2%), Space: 347.6 MB (5.48%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,25 +1,78 @@
 class Solution {
 public:
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int>components(n,-1);
-        int compid=0;
-        components[0]=compid;
 
-        for(int i=1;i<n;i++){
-            if(nums[i]-nums[i-1] >maxDiff){
-                compid++;
+     // phala jo nums diya h usi help se graph bana le 
+     // graph banana ka baad  har ek edge pe dfs chalenga phir usko store karenga
+
+    void dfs(int node,int cnt,vector<vector<int>>&adj,vector<int>&component){
+        //first node
+        component[node]=cnt;
+
+        for(auto ngbr:adj[node]){
+            if(component[ngbr]==-1){
+                dfs(ngbr,cnt,adj,component);
             }
-                components[i]=compid;
+        }
+    }
+
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        // step-1
+        // vector<int>components(n,-1);
+        // int compid=0;
+        // components[0]=compid;
+
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]-nums[i-1] >maxDiff){
+        //         compid++;
+        //     }
+        //         components[i]=compid;
             
+        // }
+
+        // vector<bool>result;
+
+        // for(auto &q:queries){
+        //     int u=q[0];
+        //     int v=q[1];
+
+        //     result.push_back(components[u]==components[v]);
+        // }
+
+        // return result;
+
+
+
+
+        // step-2 ----->using dfs
+
+
+        vector<vector<int>>adj(n);
+
+        //build a graph
+        for(int i=1;i<n;i++){
+            if(nums[i]-nums[i-1] <=maxDiff){
+                adj[i].push_back(i-1);
+                adj[i-1].push_back(i);
+            }
+        }
+
+        vector<int>component(n,-1);
+        int cnt=0;
+
+        for(int i=0;i<n;i++){
+            if(component[i]==-1){
+                dfs(i,cnt,adj,component);
+                cnt++;
+            }
         }
 
         vector<bool>result;
 
-        for(auto &q:queries){
+        for(auto q:queries){
             int u=q[0];
             int v=q[1];
 
-            result.push_back(components[u]==components[v]);
+            result.push_back(component[u]==component[v]);
         }
 
         return result;
