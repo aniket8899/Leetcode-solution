9762b3555f995181f09e131c6b83936137378326
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-07-24 21:16:11 +0530

    Time: 2015 ms (41.57%), Space: 98.4 MB (28.09%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n=nums.size();
+
+       unordered_set<int>s1;
+
+       for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            s1.insert(nums[i]^nums[j]);
+        }
+       }
+
+       unordered_set<int>s2;
+
+       for(int pair:s1){
+        for(int &num:nums){
+            s2.insert(pair^num);
+        }
+       }
+
+       return s2.size();
+        
+    }
+};
