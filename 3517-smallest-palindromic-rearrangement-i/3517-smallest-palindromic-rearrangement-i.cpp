0d793e0fdbf00874abfa102f46b500265eda9f1f
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-07-29 01:30:38 +0530

    Time: 49 ms (49.89%), Space: 53.9 MB (98.48%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        
+
+        int n=s.size();
+        int mid=n/2;
+
+        
+
+        string ans="";
+
+        sort(s.begin(),s.begin()+mid);
+
+        for(int i=0;i<mid;i++){
+            s[n-i-1]=s[i];
+        }
+        return s;
+    }
+};
