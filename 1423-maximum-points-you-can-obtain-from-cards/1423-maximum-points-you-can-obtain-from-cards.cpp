4f2b4a0aabbcfe789e8fa7ab867cfd669cb0b0bc
Aniket Singh <166163443+aniket8899@users.noreply.github.com>
2026-09-25 14:55:07 +0530

    Time: 1898 ms (3.18%), Space: 48.5 MB (5.48%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,29 +1,63 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        
-        int n=cardPoints.size();
-        int maxi=INT_MIN;
-        int lsum=0;
-        int rsum=0;
+/*
+first traverse leftsum 1,3,6
 
-        for(int i=0;i<k;i++){
-            lsum+=cardPoints[i];
-            maxi=lsum;
+right sum
+
+
+
+
+
+*/
+    int maxScore(vector<int>& cardPoints, int k) {
+
+        int n=cardPoints.size();
+        
+        vector<int>leftsum(k+1,0);
+
+        vector<int>rightsum(k+1,0);
+
+        int maxi=INT_MIN;
+
+        // leftsum[0]=cardPoints[0];
+
+        for(int i=1;i<=k;i++){
+            leftsum[i]=leftsum[i-1]+cardPoints[i-1];
+         cout<<"leftsum"<<leftsum[i]<<endl;
         }
 
-      int rindex=n-1;
+        // 0,100,140,157
 
-      for(int i=k-1;i>=0;i--){
-        lsum=lsum-cardPoints[i];
 
-        rsum=rsum+cardPoints[rindex];
-        rindex=rindex-1;
+        // rightsum[0]=cardPoints[n];
 
-        maxi=max(maxi,lsum+rsum);
-      }
+        for(int i=1;i<=k;i++){
+            rightsum[i]=rightsum[i-1]+cardPoints[n-i];
 
-      return maxi;
+          cout<<"rightsum"<<rightsum[i]<<endl;
+        }
+
+        // 0,75,148,157
+
+
+        for(int i=0;i<=k;i++){
+            int right=k-i;
+
+            cout<<"right"<<right<<endl;
+
+            int sum=leftsum[i]+rightsum[right];
+
+            cout<<"sum"<<sum<<endl;
+
+            if(sum>maxi){
+                maxi=sum;;
+            }
+        }
+
+        return maxi;
+
 
     }
+
 };
